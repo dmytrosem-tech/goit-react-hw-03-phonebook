6d03c96104bba06cb3e0bd8eafb5e0adac5553ef
Dmytro Semeruk <deemoon83@gmail.com>
2021-10-11 14:00:16 +0300

    Updates

---
+++ static/js/main.f555956b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-03-phonebook"]=this["webpackJsonpgoit-react-hw-03-phonebook"]||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),o=n.n(c),r=n(3),i=n.n(r),s=(n(8),n(0));var a=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("p",{children:"Lol"})})};n(10);i.a.render(Object(s.jsx)(o.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))},8:function(e,t,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.f555956b.chunk.js.map
